8d52fe1b48be8498a9fa22b6febb1d097dc35066
Robert Devlin <robert.devlin@bizreach.co.jp>
2019-05-22 10:42:00 +0900

    Welcome Servlet 1

---
+++ src/la/practice/WebServlet01.java
@@ -0,0 +1,24 @@
+package la.practice;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/welcome")
+public class WebServlet01 extends HttpServlet {
+
+    public WebServlet01(){ super(); }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Welcome</title></head><body>");
+        out.println("Welcome to the first page.");
+        out.println("</body></html>");
+    }
+
+}
