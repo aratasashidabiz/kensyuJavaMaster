7ab3eee3684e3c8d99f529071494545addb05510
Jotaro.Yuza <jotaro.yuza@bizreach.co.jp>
2019-05-22 11:05:19 +0900

    ex01を解いた

--- src/la/practice/ex01/WelcomeServlet.java
+++ src/la/practice/ex01/WelcomeServlet.java
@@ -1,7 +1,23 @@
 package la.practice.ex01;
 
-public class WelcomeServlet {
-    public static void main(String[] args) {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class WelcomeServlet extends HttpServlet {
+    public WelcomeServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><meta charset=UTF=8><title>Welcome</title><body>");
+        out.println("<h1>Welcome Servlet</h1><p>It is subclass of HttpServlet</p>");
+        out.println("</body></html>");
     }
 }
